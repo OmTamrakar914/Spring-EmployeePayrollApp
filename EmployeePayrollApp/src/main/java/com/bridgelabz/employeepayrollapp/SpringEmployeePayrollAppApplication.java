ed22887bb3a3a6aedd88be9979c1bfbe02a3e7bf
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-27 16:35:02 +0530

    [Om] Add. Used Lombok Library for Logging

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/SpringEmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/SpringEmployeePayrollAppApplication.java
@@ -1,16 +1,21 @@
 package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
 public class SpringEmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEmployeePayrollAppApplication.class, args);
+		
+   SpringApplication.run(SpringEmployeePayrollAppApplication.class, args);
 	    log.info("Employee Payroll App Started");
+	    		
+	    
 	}
 
 }
