65276dc54e80a7b807396b0a75bda3d9e3f054be
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-03-01 02:00:00 +0530

    [Om] Add. Added remaining properties to the Payroll DTO and Model

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.employeepayrollapp.dto;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 import jakarta.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeDTO {
+public @ToString class EmployeeDTO {
+	
 	
   @NotEmpty(message = "Employee name cannot br null")
   @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
@@ -14,4 +19,13 @@ public class EmployeeDTO {
   @Min(value=500,message = "Min Wage should be more than 500")
   private double salary;
   
+  public String gender;
+  
+  public LocalDate startDate;
+  
+  public String note;
+  
+  public String profilPic;
+  
+  public List<String> department;
 }
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -6,25 +6,39 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.*;
+
 @Entity
-public class EmployeePayrollData {
+public @Data class EmployeePayrollData {
 
 	
-	@Id
+ 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	
 	private String name;
-	private int id;
+	//private int id;
 	private double salary;
+	private String gender;
+	private LocalDate startDate;
+	private String note;
+	private String profilePic;
+	private List<String> departments;
 	
 	public EmployeePayrollData() {
 		
 	}
 	
 	public EmployeePayrollData(int id, EmployeeDTO employeedto) {
-		this.id = id;
+		//this.id = id;
 		this.name = employeedto.getName();
 		this.salary = (double) employeedto.getSalary();
+		this.gender = employeedto.gender;
+		this.note = employeedto.note;
+		this.startDate = employeedto.startDate;
+		this.profilePic = employeedto.profilPic;
+		this.departments = employeedto.department;
+		
 	}
 	
 	public String getName() {
@@ -43,8 +57,8 @@ public class EmployeePayrollData {
 		this.salary = salary;
 	}
 	
-	public int getEmployeeId() {
-		return id;
-	}
+//	public int getEmployeeId() {
+//		return id;
+//	}
 	
 }
