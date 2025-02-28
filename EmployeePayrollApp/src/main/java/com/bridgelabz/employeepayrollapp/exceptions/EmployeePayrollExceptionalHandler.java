5551c1851a4e6c617dc51c6a2702a37fce0dff13
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-28 16:25:30 +0530

    [Om] Add. Added user friendly exception messege while handling

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionalHandler {
+ 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgNotValidException(MethodArgumentNotValidException exception){
+		
+		List<ObjectError> errorList =  exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+		
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errMsg);
+		
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+}
